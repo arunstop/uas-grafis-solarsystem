cc8a04f3fc6ecf0b821e1e6c84566017868452d7
bibiehadi <bibie.hadi22@gmail.com>
2019-07-17 12:17:41 +0700

    fixed imageloader error

--- uas_grafis/main.cpp
+++ uas_grafis/main.cpp
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <iostream>
-#include "imageloader.h"
+#include "imgloader.h"
 
 GLUquadric *sun;
 GLuint sunTexture;
--- uas_grafis/src/imgloader.cpp
+++ uas_grafis/src/imgloader.cpp
@@ -4,7 +4,7 @@
 
 
 
-#include "imageloader.h"
+#include "imgloader.h"
 
 
 
